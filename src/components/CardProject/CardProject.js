c09ccd3828fd3316af44830484677eff24c5f10b
2Kelvin <rocketmancodes02@gmail.com>
2024-03-02 15:16:55 +0300

    style add for project cards

--- src/components/CardProject/CardProject.js
+++ src/components/CardProject/CardProject.js
@@ -5,13 +5,18 @@ export default function CardProject({ appType, appName, githubLink, description,
     return (
         <div className='projectCard'>
             <code>{appType}</code>
+
             <div className='appHeadingDiv'>
                 <h3>{appName}</h3>
                 <a href={githubLink} target='blank'><LuGithub className='githubIcon' /></a>
             </div>
+
             <div className='appDescription'>{description}</div>
+
             <div className='appLanguages'>
-                {appTags.map((tag) => <code>{tag}</code>)}
+                <div className='langSubdiv'>
+                    {appTags.map((tag) => <code>{tag}</code>)}
+                </div>
             </div>
         </div>
     );
